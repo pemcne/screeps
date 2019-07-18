0222c803a96d11853c4da76804d9e29e723639d5
Peter McNeill <pemcneill8@gmail.com>
2019-07-18 08:18:47 -0400

    Changed worker to action model

--- src/creeps/roles/worker.js
+++ src/creeps/roles/worker.js
@@ -1,44 +1,20 @@
 import { BaseCreep } from "../base";
 
 export class Worker extends BaseCreep {
-  constructor(creep) {
-    super(creep);
-    if (!this.creep.memory.worker) {
-      this.init();
-    }
+  static getInitialActions() {
+    const controller = this.creep.room.controller;
+    return [{
+      type: 'upgrade',
+      data: {
+        target: controller.id
+      },
+      repeat: true
+    }];
   }
-  init() {
-    this.creep.memory.worker = {
-      working: false
-    };
+  get available() {
+    return this.creep.memory.available;
   }
-  run() {
-    if (this.creep.memory.worker.working && this.isEmpty()) {
-      this.creep.memory.worker.working = false;
-    }
-    if (!this.creep.memory.worker.working && this.isFull()) {
-      this.creep.memory.worker.working = true;
-    }
-    if (this.creep.memory.worker.working) {
-      const controller = this.creep.room.controller;
-      if (controller.ticksToDowngrade < 3000) {
-        this.upgradeController();
-      }
-      const repair = this.creep.pos.findClosestByPath(FIND_STRUCTURES, {
-        filter: (s) => s.hits < s.hitsMax / 2 && s.structureType !== STRUCTURE_WALL
-      })
-      if (repair !== null) {
-        return this.repair(repair);
-      }
-      const site = this.findClosestConstructionSite();
-      if (site !== null) {
-        return this.build(site);
-      } else {
-        return this.upgradeController();
-      }
-    } else {
-      return this.findEnergy();
-    }
-
+  set available(avail) {
+    this.creep.memory.available = avail;
   }
 }
