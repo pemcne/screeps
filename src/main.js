eacbc92ee138502f22e901baeb997091bc06da52
pemcne <pemcneill8@gmail.com>
2019-07-27 10:17:02 -0400

    Base init working

--- src/main.js
+++ src/main.js
@@ -4,17 +4,11 @@ import BaseManager from './managers/base';
 export const loop = () => {
   if (!Memory.bases) {
     Memory.bases = {}
+    BaseManager.newBase('base1', Object.values(Game.rooms)[0])
   }
   for (name in Memory.bases) {
     console.log(name);
     const base = new BaseManager(name);
     base.run();
   }
-  // RoomPlanner.scan();
-  // CreepController.run();
-
-  // if (Game.spawns.Spawn1.spawning) {
-  //   const spawningCreep = Game.spawns.Spawn1.spawning.name;
-  //   console.log(`Spawning ${spawningCreep}`);
-  // }
 }
--- src/managers/base.js
+++ src/managers/base.js
@@ -1,26 +1,20 @@
 import RoomManager from './room';
 
 class BaseManager {
-  constructor(name, room) {
+  constructor(name) {
     this.name = name;
-    const baseObj = Memory.bases[this.name];
-    if (Object.entries(baseObj).length === 0 && baseObj.constructor === Object) {
-      // This is a new base so init
-      this.init();
-    } else {
-      this.load();
-    }
-    this.save();
+  }
+  static newBase(name, room) {
+    Memory.bases[name] = {
+      'rooms': [room.name]
+    };
   }
   get rooms() {
-    let rooms = Memory.bases[this.name].rooms;
-    if (!rooms) {
-      return Object.values(Game.rooms);
-    }
-    return rooms;
+    const rooms = Memory.bases[this.name].rooms;
+    return _.map(rooms, (r) => Game.rooms[r]);
   }
   set rooms(rooms) {
-    const save = _.map(rooms, 'id');
+    const save = _.map(rooms, 'name');
     Memory.bases[this.name].rooms = save;
   }
   get spawns() {
@@ -28,10 +22,10 @@ class BaseManager {
     if (!spawns) {
       return this.rooms[0].find(FIND_MY_SPAWNS);
     }
-    return spawns;
+    return _.map(spawns, (s) => Game.spawns[s]);
   }
   set spawns(spawns) {
-    const save = _.map(spawns, 'name');
+    const save = _.map(spawns, 'id');
     Memory.bases[this.name].spawns = save;
   }
   get creeps() {
@@ -39,36 +33,17 @@ class BaseManager {
     if (!creeps) {
       return [];
     }
-    return creeps;
+    return _.map(creeps, (c) => Game.creeps[c]);
   }
   set creeps(creeps) {
-    const save = _.map(creeps, 'name');
+    const save = _.map(creeps, 'id');
     Memory.bases[this.name].creeps = save;
-  }
-  init() {
-    this.rooms = Object.values(Game.rooms);
-    this.spawns = this.rooms[0].find(FIND_MY_SPAWNS);
-    this.creeps = [];
-    Memory.bases[this.name] = {};
-  }
-  load() {
-    this.rooms = _.map(Memory.bases[this.name].rooms, (r) => Game.rooms[r]);
-    this.spawns = _.map(Memory.bases[this.name].spawns, (s) => Game.spawns[s]);
-    this.creeps = _.map(Memory.bases[this.name].creeps, (c) => Game.creeps[c]);
-  }
-  save() {
-    const rooms = _.map(this.rooms, 'name');
-    const spawns = _.map(this.spawns, 'name');
-    const creeps = _.map(this.creeps, 'id');
-    Memory.bases[this.name].rooms = rooms;
-    Memory.bases[this.name].spawns = spawns;
-    Memory.bases[this.name].creeps = creeps;
   }
   run() {
     this.rooms.forEach((room) => {
       const roomPlanner = new RoomManager(this, room);
       roomPlanner.scan();
-    })
+    });
   }
 }
 
